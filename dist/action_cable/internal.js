bca688f5a55c40ed134cc5ae9b8e9f29f7d63df6
Zach Schneider <zach@schneid.io>
2016-04-23 18:07:18 -0500

    Add built files

--- dist/action_cable/internal.js
+++ dist/action_cable/internal.js
@@ -1,1 +1,1 @@
-module.exports={identifiers:{ping:"_ping"},message_types:{confirmation:"confirm_subscription",rejection:"reject_subscription"}};
+module.exports={identifiers:{ping:"_ping"},message_types:{welcome:"welcome",ping:"ping",confirmation:"confirm_subscription",rejection:"reject_subscription"}};
